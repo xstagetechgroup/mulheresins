77889bd4048c57426cba9755dbc746498d880006
Eduardo Brito <eduardo370@outlook.pt>
2025-06-30 01:05:05 +0100

    website brevemente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,40 @@
 import Image from "next/image";
+import fundo from '@/assets/2.jpg'
+import whiteLogo from '@/assets/logo-horizontal-white.png'
+import { FaFacebook, FaInstagramSquare, FaLinkedinIn } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-full h-screen relative">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <Image
+        src={fundo}
+        alt="Iracelma Almeida"
+        width={900}
+        height={900}
+        className="w-full h-full object-cover object-center absolute z-10 "
+      />
+      <div className="w-full h-full px-10 py-5 flex flex-col justify-center items-center absolute z-20 bg-black/80">
+        <div className="w-full ">
+          <Image
+            src={whiteLogo}
+            alt="Logo"
+            width={200}
+            height={200}
+            className="w-40"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className=" w-full h-full border-8 border-black shadow-2xl text-white flex flex-col justify-center items-center uppercase text-center gap-10 text-lg">
+          <p>A Resiliência Não é <br /> ser forte o tempo todo. <br /> É saber levantar-se <br /> com propósito.</p>
+          <p className="text-5xl md:text-9xl ">A M I</p>
+          <p>· Um Novo Website Brevemente ·</p>
+        </div>
+        <div className="text-white text-lg w-full flex gap-4 items-center justify-start pt-4">
+          <a target="_blank" href={'https://www.facebook.com/share/16SHLs8tHr/'}><FaFacebook /></a>
+          <a target="_blank" href={'https://www.instagram.com/associacaomulheresinstruidas?igsh=MWwycXlwOWVseGwzdA=='}><FaInstagramSquare /></a>
+          <a target="_blank" href={'https://www.linkedin.com/in/iracelma-almeida-8880a132a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'}><FaLinkedinIn /></a>
+        </div>
+      </div>
     </div>
   );
 }
